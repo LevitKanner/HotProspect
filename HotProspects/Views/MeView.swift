226bc8a2be2d8809953e1a75eea922003ae903a4
Levit Osei Kanner <lkanner21@gmail.com>
2019-12-15 20:30:23 +0000

    Constructs the user profile view

--- HotProspects/Views/MeView.swift
+++ HotProspects/Views/MeView.swift
@@ -7,11 +7,61 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct MeView: View {
+    @State private var name = ""
+    @State private var email = ""
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                Form{
+                    TextField("name", text: $name)
+                        .textContentType(.name)
+                        .font(.title)
+                        .padding(.all)
+                    
+                    TextField("email", text: $email)
+                        .textContentType(.emailAddress)
+                        .font(.title)
+                        .padding(.all)
+                    
+                }
+                .frame(height: 250)
+                
+                Image(uiImage: generateQrCode(from: "\(name)\n\(email)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200 , height: 200)
+                    .padding(.top , 90)
+                
+                Spacer()
+            }
+            .navigationBarTitle("Your code", displayMode: .inline)
+        }
     }
+    
+    
+    //Generate qrcode using core Image
+    func generateQrCode(from string: String) -> UIImage{
+        //converts input string into data
+        let data = Data(string.utf8)
+        //Sets value for filter
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage{
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent){
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+        
+    }
+    
 }
 
 struct MeView_Previews: PreviewProvider {
